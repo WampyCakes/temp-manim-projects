b2c58ae2d43935f4a53f435a1cca79bc69f9bb45
KingWampy <superpie@usa.com>
2021-01-01 01:58:19 -0500

    Update create.js

--- .github/scripts/create.js
+++ .github/scripts/create.js
@@ -1,2 +1,15 @@
-var fs = require('fs');
-fs.writeFileSync('submission.json', JSON.stringify(process.env.DATA));
+var fs = require('fs')
+const args = process.argv.slice(2)
+data = {
+    "date": args[0],
+    "title": args[1],
+    "author": args[2],
+    "credit": args[3],
+    "video_url": args[4],
+    "description": args[5],
+    "source": args[6],
+    "fields": args[7],
+    "subfields": args[8],
+    "tags": args[9],
+}
+fs.writeFileSync('submission.json', JSON.stringify(data))
