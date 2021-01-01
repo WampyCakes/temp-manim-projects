c709f56114311f23aa28af6f5c4e79740c6baf38
KingWampy <superpie@usa.com>
2021-01-01 18:52:19 -0500

    Update add-entry.js

--- .github/scripts/add-entry.js
+++ .github/scripts/add-entry.js
@@ -1,16 +1,19 @@
 var fs = require('fs')
-var submissions = JSON.parse(fs.readFileSync('submissions.json'))
-var newSubmission = JSON.parse(fs.readFileSync('submissions/'+process.argv[2]+'.json'))
+var submissions = require('./submissions.json')
+var submission = require('./submissions/'+process.argv[2]+'.json')
+var newSubmission = []
+// var submissions = JSON.parse(fs.readFileSync('submissions.json'))
+// var newSubmission = JSON.parse(fs.readFileSync('submissions/'+process.argv[2]+'.json'))
 
-Object.keys(newSubmission).forEach((key) => {
-    newSubmission[key] = Object.values(newSubmission[key]) 
+Object.values(submission).forEach((value) => {
+    newSubmission.push(value)
 })
 
 submissions.push(newSubmission)
 
 // If sorting performance becomes an issue, look into https://github.com/Pimm/mapsort (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Sorting_with_map)
 function date_sort(a, b) {
-    return new Date(b.date) - new Date(a.date)
+    return new Date(b[0]) - new Date(a[0])
 }
 
 submissions.sort(date_sort)
