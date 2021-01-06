3b87e64d9cea3f9821e5d9fb797ae0889685e398
KingWampy <superpie@usa.com>
2021-01-06 15:42:28 -0500

    Update add-entry.js

--- .github/scripts/add-entry.js
+++ .github/scripts/add-entry.js
@@ -1,17 +1,32 @@
+// This script is called by the add-entry.yml workflow
+
+const { request } = require('@octokit/request')
 var fs = require('fs')
+// For some reason unknown to me, requiring the JSON files throws a Module Not Found Error
+// This works locally, but not on GitHub. Why??
 // var submissions = require('./submissions.json')
-// var submission = require('./submissions/'+process.argv[2]+'.json')
 var submissions = JSON.parse(fs.readFileSync('submissions.json'))
-var submission = JSON.parse(fs.readFileSync('submissions/'+process.argv[2]+'.json'))
+var submission = get_data(args[2], args[3])
 var newSubmission = []
 
+// Because we keep the key:value pairs in the same order throughout the whole process,
+// we can remove the keys. Between this and stringifying without extra whitespace, my
+// testing has shown this will reduce the size of the submissions.json file by roughly 90%.
+// Very important so that we a) can fit roughly twice as many submissions before hitting the
+// GitHub file size limit and b) reduce loading time of the website.
 Object.values(submission).forEach((value) => {
     newSubmission.push(value)
 })
 
 submissions.push(newSubmission)
 
-// If sorting performance becomes an issue, look into https://github.com/Pimm/mapsort (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Sorting_with_map)
+// If sorting performance becomes an issue, look into https://github.com/Pimm/mapsort
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Sorting_with_map)
+// We want to sort here so that lazyily reading the submissions.json file on the website returns
+// the newest entries by default (or oldest if so desired). This saves time later by not having
+// to sort the data the website receives. Additionally, because the sort will only ever have one
+// entry out of order, it *shouldn't* become an issue. This seems an easier method than trying
+// to push the new entry into the correct index by checking dates.
 function date_sort(a, b) {
     return new Date(b[0]) - new Date(a[0])
 }
@@ -19,3 +34,14 @@ function date_sort(a, b) {
 submissions.sort(date_sort)
 
 fs.writeFileSync('submissions.json', JSON.stringify(submissions))
+
+async function get_data(number, repo) {
+    await request(`GET /repos/${repo}/issues/{issue_number}`, {
+        issue_number: number
+    }).then(response => {
+        return JSON.parse(response.data.body.split('yaml\n')[1].split('```')[0]);
+    }).catch((error) => {
+        console.log(error);
+        return 'Error'
+    })
+}
