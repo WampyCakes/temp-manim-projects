c31d97c739183133149bcd0bc87564c72b711872
WampyCakes <WampyCakes@users.noreply.github.com>
2021-02-03 17:02:26 -0500

    Updates

--- scripts/add-video.js
+++ scripts/add-video.js
@@ -1,11 +1,15 @@
-// This script is called by the add-video.yml workflow
+// This script is called by the add-video.yml workflow; it updates the submissions.json file
 
 const { request } = require('@octokit/request')
 var fs = require('fs')
 
+// An immediately invoked function expressed threw an error here before for some reason. Maybe it was incorrect syntax..
 run();
 async function run() {
+    // Read the body of the submission issue
     await request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+        // owner and repo have to be split because directly injecting ${{ github.repository }} changes the '/' character.
+        // Printing process.argv[3] displays it correctly, but it doesn't work here.
         owner: process.argv[3].split('/')[0],
         repo: process.argv[3].split('/')[1],
         issue_number: process.argv[2]
@@ -14,8 +18,12 @@ async function run() {
         // This works locally, but not on GitHub. Why??
         // var submissions = require('submissions.json')
         var submissions = JSON.parse(fs.readFileSync('submissions.json'))
+        
+        // This is the JSON contained in the issue body.
         var submission = JSON.parse(response.data.body.replace('\r', '').split('yaml\n')[1].split('```')[0]);
+        
         var newSubmission = []
+        
         // Because we keep the key:value pairs in the same order throughout the whole process,
         // we can remove the keys. Between this and stringifying without extra whitespace, my
         // testing has shown this will reduce the size of the submissions.json file by roughly 90%.
