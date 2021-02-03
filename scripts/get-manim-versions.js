8e02c5dda919f177105022b433676452b500139f
WampyCakes <WampyCakes@users.noreply.github.com>
2021-02-03 18:30:33 -0500

    Update get-manim-versions.js

--- scripts/get-manim-versions.js
+++ scripts/get-manim-versions.js
@@ -1,18 +1,21 @@
 const https = require('https')
 const fs = require('fs')
 
-console.log('test');
 if(new Date().getHours() == 0){
-    console.log('Checking PyPi for releases');
-    https.get('https://pypi.org/pypi/manim/json', (response) => {
-        let data = '';
+    try {
+        console.log('Checking PyPi for Manim releases');
+        https.get('https://pypi.org/pypi/manim/json', (response) => {
+            let data = '';
 
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
 
-        response.on('end', () => {
-            fs.writeFileSync('manim-versions.json', JSON.stringify(Object.keys(JSON.parse(data).releases).reverse()))
-        })
-    })
+            response.on('end', () => {
+                fs.writeFileSync('manim-versions.json', JSON.stringify(Object.keys(JSON.parse(data).releases).reverse()))
+            })
+        })       
+    } catch (error) {
+        console.log(error);    
+    }
 }
